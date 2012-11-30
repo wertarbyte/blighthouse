c7359f34da7834ab18f6c6fd1be7d1484995b293
Stefan Tomanek <tomanek@internet-sicherheit.de>
2012-11-30 18:34:34 +0100

    construct entire frame on-the-fly

--- blighthouse.c
+++ blighthouse.c
@@ -5,45 +5,8 @@
 #include <unistd.h>
 #include <string.h>
 
-char beacon_pre_ssid[] = 
-/* IEEE802.11 */
-"\x80\x00\x00\x00"
-/* destination */
-"\xff\xff\xff\xff\xff\xff"
-/* source */
-"\x00\x16\x3e\x1c\x4a\x3f"
-/* BSSID */
-"\x00\x16\x3e\x1c\x4a\x3f"
-/* sequence number */
-"\x70\x3e"
-/* timestamp */
-"\x80\x61\x17\x06\x00\x00\x00\x00"
-/* beacon interval */
-"\x64\x00"
-/* capabilities */
-"\x01\x04"
-;
-
-/* tagged params */
-/* tag 0: SSID */
-//"\x00"
-/* tag length */
-//"\x08"
-/* ESSID */
-//"\x6d\x65\x69\x6e\x6e\x65\x74\x7a"
-
-char beacon_post_ssid[] =
-/* supported rates */
-"\x01\x08\x82\x84\x8b\x96\x0c\x12\x18\x24"
-/* current channel */
-"\x03\x01\x01"
-/* traffic indication map */
-"\x05\x04\x01\x02\x00\x00"
-/* ERP information */
-"\x2a\x01\x04"
-/* Extended supported rates */
-"\x32\x04\x30\x48\x60\x6c"
-;
+static uint8_t ap_mac[6]    = "\x00\x16\x3e\x1c\x4a\x3f";
+static uint8_t timestamp[8] = "\x80\x61\x17\x06\x00\x00\x00\x00";
 
 static char *append_to_buf(char *buf, char *data, int size) {
 	memcpy(buf, data, size);
@@ -61,11 +24,21 @@ int build_beacon(char *buf, char *essid) {
 	memset(b, 0x00, 8);
 	b[2] = 8;
 	b += 8;
-	b = append_to_buf(b, beacon_pre_ssid, sizeof(beacon_pre_ssid)-1);
-	*(b++) = 0; // tag essid
+	b = append_to_buf(b, "\x80\x00\x00\x00", 4); /* IEEE802.11 beacon frame */
+	b = append_to_buf(b, "\xff\xff\xff\xff\xff\xff", 6); /* destination */
+	b = append_to_buf(b, ap_mac, sizeof(ap_mac)); /* source */
+	b = append_to_buf(b, ap_mac, sizeof(ap_mac)); /* BSSID */
+	b = append_to_buf(b, "\x00\x00", 2); /* sequence number */
+	b = append_to_buf(b, timestamp, sizeof(timestamp)); /* time stamp */
+	b = append_to_buf(b, "\x64\x00", 2); /* beacon interval */
+	b = append_to_buf(b, "\x01\x04", 2); /* capabilities */
+
+	*(b++) = 0; /* tag essid */
 	*(b++) = strlen(essid);
 	b = append_str(b, essid);
-	b = append_to_buf(b, beacon_post_ssid, sizeof(beacon_post_ssid)-1);
+
+	b = append_to_buf(b, "\x01\x01\x82", 3); /* We only support 1 MBit */
+	b = append_to_buf(b, "\x03\x01\x01", 3); /* we are on channel 1 */
 	return (b-buf);
 }
 
